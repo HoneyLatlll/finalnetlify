bbbf3312b7d60647d71c36d8ad88e7663a19b417
HoneyLatelll <165013358+HoneyLatlll@users.noreply.github.com>
2024-07-13 17:31:01 +0900

    Update fetch_naver_it_news.py

--- fetch_naver_it_news.py
+++ fetch_naver_it_news.py
@@ -3,19 +3,18 @@ from bs4 import BeautifulSoup
 import json
 
 def fetch_naver_it_news():
-    url = 'https://newsstand.naver.com/?list=ct4'  # 네이버 IT 뉴스 URL
+    url = 'https://news.naver.com/main/list.naver?mode=LSD&mid=sec&sid1=105'  # 네이버 IT 뉴스 URL
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
     news_items = []
     
-    # 네이버 뉴스스탠드의 IT 뉴스 항목을 크롤링 (적절한 CSS 선택자 사용)
-    articles = soup.select('div._4F2Kn article')
-    for article in articles:
-        title = article.select_one('strong').text
-        link = article.select_one('a')['href']
-        image_url = article.select_one('img')['src']
-        news_items.append({'title': title, 'link': link, 'image_url': image_url})
+    # 네이버 IT 뉴스 페이지의 헤드라인을 크롤링 (적절한 CSS 선택자 사용)
+    headlines = soup.select('ul.type06_headline li dl dt:not(.photo) a')
+    for headline in headlines:
+        title = headline.text.strip()
+        link = headline['href']
+        news_items.append({'title': title, 'link': link})
     
     return news_items
 
